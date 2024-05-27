35d35e3c9834f8a68d34e02593e0f901e819a20e
heesu21 <gmltncpfl1@naver.com>
2024-05-27 23:42:24 +0900

    1st

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useState } from "react";
+import "./Count.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const addCount = () => setCount(count + 1);
+
+  const addCount2 = () => setCount(count - 1);
+
+  const addCount3 = () => setCount(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      Count: {count}
+      <Fragment>
+        <button className="btn" onClick={addCount}>
+          +
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <button className="btn" onClick={addCount2}>
+          -
+        </button>
+        <button className="btn" onClick={addCount3}>
+          reset
+        </button>
+      </Fragment>
+    </div>
+  );
 }
 
-export default App
+export default App;
